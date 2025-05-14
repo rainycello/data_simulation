21db3c26f5f1ce9a5337ca0a155df8fc13aa93a5
rainycello <dmyboi2@gmail.com>
2025-05-14 20:04:43 +0200

    remove comments

--- data_simulation.py
+++ data_simulation.py
@@ -1,28 +1,36 @@
 import pandas as pd
 import numpy as np
 
+
 n_samples = 200
 proportion_sick = 0.5
 n_sick = int(n_samples * proportion_sick)
 n_healthy = n_samples - n_sick
 
+
 sick_data = pd.DataFrame({
-    "wiek": np.random.normal(45, 10, n_sick).astype(int),
+    "wiek": np.clip(np.random.normal(45, 10, n_sick).astype(int), 0, 100),
     "płeć": np.random.choice(["M", "K"], size=n_sick),
-    "temperatura": np.random.normal(38.5, 0.5, n_sick),
-    "ciśnienie": np.random.normal(110, 10, n_sick),
+    "temperatura": np.clip(np.random.normal(38.5, 0.5, n_sick), 35, 42),
+    "ciśnienie": np.clip(np.random.normal(110, 10, n_sick), 70, 200),
     "choroba": 1
 })
 
+
 healthy_data = pd.DataFrame({
-    "wiek": np.random.normal(45, 10, n_healthy).astype(int),
+    "wiek": np.clip(np.random.normal(45, 10, n_healthy).astype(int), 0, 100),
     "płeć": np.random.choice(["M", "K"], size=n_healthy),
-    "temperatura": np.random.normal(36.6, 0.3, n_healthy),
-    "ciśnienie": np.random.normal(120, 10, n_healthy),
-    "chory": 0
+    "temperatura": np.clip(np.random.normal(36.6, 0.3, n_healthy), 35, 42),
+    "ciśnienie": np.clip(np.random.normal(120, 10, n_healthy), 70, 200),
+    "choroba": 0
 })
+
 
 data = pd.concat([sick_data, healthy_data], ignore_index=True)
 data = data.sample(frac=1).reset_index(drop=True)
 
-print(data.head())
+
+assert not data.isnull().values.any(), "Data contains NaNs!"
+
+#print(data.head())
+print(data)
